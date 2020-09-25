4989ef53dafa6028492c55c25edff5ad0397086d
AnthonyH <flohoa-a@outlook.com>
2020-09-25 13:23:40 +0200

    Exercice 1

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,68 @@
 <template>
   <div id="app">
-
+    <div class="moon"></div>
+    <div v-for="star in stars" v-bind:key="star.id">
+      <div class="star" :style="`left: ${star.x}px; top:${star.y}px`"></div>
+    </div>
   </div>
 </template>
 
+
 <script>
-export default {
-  name: "app"
-};
+  import {randomX, randomY} from "./helpers/random"
+  export default {
+    name: "app",
+    data() {
+      return {
+        stars: this.generateStars(100)
+      }
+    },
+    methods: {
+      generateStars(nbStars) {
+        const allStars = []
+        for(let i = 0; i < nbStars; i++){
+            allStars.push({x: randomX(), y:randomY()})
+        }
+        return allStars;
+      }
+    }
+  };
 </script>
 
 <style lang="scss">
-body {
-  margin: 0;
-}
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100vh;
-  width: 100vw;
-  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
-}
+  body {
+    margin: 0;
+  }
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100vh;
+    width: 100vw;
+    background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+  }
 
-.star {
-  position: absolute;
-  background-color: white;
-  border-radius: 100%;
-  height: 4px;
-  width: 4px;
-}
+  .star {
+    position: absolute;
+    background-color: white;
+    border-radius: 100%;
+    height: 4px;
+    width: 4px;
+  }
 
-.moon {
-  position: absolute;
-  height: 100px;
-  width: 100px;
-  background-image: url("https://3dexport.com/items/2015/02/20/385337/100201/moon_texture_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1258985_o.png");
-  background-size: cover;
-  box-shadow: 0px 0px 12px 4px white;
-  filter: brightness(1.5);
-  z-index: 1;
-  right: 10%;
-  top: 15%;
-  border-radius: 100px;
-}
+  .moon {
+    position: absolute;
+    height: 100px;
+    width: 100px;
+    background-image: url("https://3dexport.com/items/2015/02/20/385337/100201/moon_texture_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1258985_o.png");
+    background-size: cover;
+    box-shadow: 0px 0px 12px 4px white;
+    filter: brightness(1.5);
+    z-index: 1;
+    right: 10%;
+    top: 15%;
+    border-radius: 100px;
+  }
 </style>
