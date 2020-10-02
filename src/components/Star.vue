29d459577ad9adaa95a2d13d45b53182dd8a9f31
AnthonyH <flohoa-a@outlook.com>
2020-10-02 10:35:16 +0200

    add computed in star component

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,20 +1,29 @@
 <template>
   <div
-    class="star"
-    :style="`top: ${getRandomY()}px; left: ${getRandomX()}px;`"
-    ></div>
+    class="star" :style="style"/>
 </template>
 
 <script>
 import { randomX, randomY } from "../helpers/random";
 export default {
     name: "Star",
-    methods: {
+    computed: {
         getRandomX() {
         return randomX();
         },
         getRandomY() {
         return randomY();
+        },
+        getRandomRadius() {
+            return Math.round(Math.random() * Math.round(4))
+        },
+        style() {
+            return `
+                top: ${this.getRandomY}px;
+                left: ${this.getRandomX}px;
+                height: ${this.getRandomRadius}px;
+                width: ${this.getRandomRadius}px;
+            `
         }
     }
     }
